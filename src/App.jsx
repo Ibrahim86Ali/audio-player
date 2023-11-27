585f77e414a1de7699e033f9799690c5db6d01fa
Ibrahim ali <124343256+Ibrahim86Ali@users.noreply.github.com>
2023-11-27 10:42:59 +0100

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-import Category from "../Component/Category/Category";
-import Soundlist from "./Components/SoundList";
+import { useState, useEffect } from "react";
+import Category from "./components/category/Category.jsx";
+import SoundList from "./components/SoundList.jsx";
+import AudioPlayer from "./components/AudioPlayer.jsx";
+import axios from "axios";
 
-const App = () => {
+export default function App() {
+  const [records, setRecords] = useState([]);
+  const [tracks, setTracks] = useState([]);
+  const [track, setTrack] = useState(null);
+
+  const fetchRecords = async () => {
+    try {
+      const response = await axios.get("http://localhost:5010/audio");
+      const data = response.data.result;
+
+      setRecords(data);
+      setTracks([...data]);
+    } catch (error) {
+      console.error("Error fetching records:", error);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    fetchRecords();
+  }, []);
+
   return (
     <>
-      <Category />
-      <Soundlist />
+      <Category records={records} setTracks={setTracks} />
+      <SoundList tracks={tracks} setTrack={(track) => setTrack(track)} />
+      <AudioPlayer track={track} />
     </>
   );
-};
-
-export default App;
+}
